fbc57bf7a0e23b6bdc5dfbfb759cd0493babd3cd
mahonya333 <mahonya333@gmail.com>
2022-08-01 02:57:25 +0300

    init project

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const TodoList = () => {
+  return (
+    <ul>
+      <li>Learn React</li>
+      <li>Build Awesome App</li>
+    </ul>
+  )
+}
+
+const AppHeader = () => {
+  return (
+    <h1>My Todo List</h1>
+  )
+}
+
+const SearchPanel = () => {
+  return (
+    <input placeholder="search"/>
+  )
+}
+
+const el = (
+  <div>
+    <AppHeader/>
+    <SearchPanel/>
+    <TodoList/>
+  </div>
+);
+
+ReactDOM.render(el, document.getElementById('root'));//Выводим на страницу в елемент root

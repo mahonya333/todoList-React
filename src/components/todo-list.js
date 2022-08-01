fcaa3e61dfb22cca4a84d05671fc7e90788d934b
mahonya333 <mahonya333@gmail.com>
2022-08-02 02:28:24 +0300

    lesson 32.How to import css

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,22 +1,23 @@
 import React from 'react';
 
-import TodoListItem from "./todo-list-item";
+import TodoListItem from './todo-list-item';
+import './todo-list.css';
 
-const TodoList = ({todos}) => {
+const TodoList = ({ todos }) => {
+
   const elements = todos.map((item) => {
-
-    const {id, ...itemProps} = item;
+    const { id, ...itemProps } = item;
 
     return (
-      <li key={id}>
-        <TodoListItem {... itemProps}/>
+      <li key={id} className="list-group-item">
+        <TodoListItem {...itemProps } />
       </li>
     );
   });
 
   return (
-    <ul>
-      {elements}
+    <ul className="list-group todo-list">
+      { elements }
     </ul>
   );
 };

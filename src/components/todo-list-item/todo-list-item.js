3801a63d13289eeff28a412c2bdf2a26d2a2fff9
mahonya333 <mahonya333@gmail.com>
2022-08-05 01:55:59 +0300

    lesson 36.State - state React component

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -7,13 +7,25 @@ export default class TodoListItem extends Component {
   constructor() {
     super();
 
+    this.state = {
+      done: false
+    };
+
     this.onLabelClick = () => {
-      console.log(this.props.label)
+      this.setState({
+        done: true
+      })
     };
   }
 
   render() {
     const {label, important = false} = this.props;
+    const {done} = this.state;
+
+    let classNames = 'todo-list-item';
+    if(done) {
+      classNames += ' done';
+    }
 
     const style = {
       color: important ? 'steelblue' : 'black',
@@ -21,7 +33,7 @@ export default class TodoListItem extends Component {
     };
 
     return (
-      <span className="todo-list-item">
+      <span className={classNames}>
       <span
         className="todo-list-item-label"
         style={style}

8d4a413465151d0e2940105875b42ddacc73da55
mahonya333 <mahonya333@gmail.com>
2022-08-21 16:45:33 +0300

    lesson 41-42.setState - create element, Data in React app

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,10 +4,13 @@ import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
 import TodoList from '../todo-list';
 import ItemStatusFilter from '../item-status-filter';
+import ItemAddForm from '../item-add-form';
 
 import './app.css';
 
 export default class App extends Component {
+
+  maxId = 100;
 
   state = {
     todoData: [
@@ -28,6 +31,29 @@ export default class App extends Component {
     });
   };
 
+  addItem = (text) => {
+    const newItem = {
+      label: text,
+      important: false,
+      id: this.maxId++
+    }
+
+    this.setState(({todoData}) => {
+      const newArr = [...todoData, newItem];
+      return {
+        todoData: newArr
+      };
+    });
+  };
+
+  onToggleImportant = (id) => {
+    console.log('toggle important', id)
+  };
+
+  onToggleDone = (id) => {
+    console.log('toggle done', id)
+  };
+
   render() {
     return (
       <div className="todo-app">
@@ -37,7 +63,12 @@ export default class App extends Component {
           <ItemStatusFilter />
         </div>
 
-        <TodoList todos={this.state.todoData} onDeleted={this.deleteItem}/>
+        <TodoList todos={this.state.todoData}
+                  onDeleted={this.deleteItem}
+                  onToggleImportant={this.onToggleImportant}
+                  onToggleDone={this.onToggleDone}
+        />
+        <ItemAddForm onItemAdded={this.addItem}/>
       </div>
     );
   };
